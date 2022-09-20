dd65b4616a9f9b03c4ef6b13e3d25426dc51de90
Arya Zand <28754724+aryazand@users.noreply.github.com>
2022-09-20 12:29:34 -0500

    output folder as command line argument

--- Scripts/process_fastq.sh
+++ Scripts/process_fastq.sh
@@ -2,14 +2,14 @@
 
 # Get Arguments
 #   - i = input fastq file
-#   - o = output fasta file 
+#   - o = output folder
 while getopts i:o: flag
 do
     case "${flag}" in
         i) input_fastq=${OPTARG};;
-        o) output_fasta=${OPTARG};;
+        o) output_folder=${OPTARG};;
     esac
 done
 
-mkdir -p ./Process_Data/Trimmed
-./Scripts/TrimGalore-0.6.0/trim_galore --paired --small_rna --dont_gzip -j 4 --ouput_dir --fastqc ./Process_Data/Trimmed $i
+mkdir -p $output_folder
+./Scripts/TrimGalore-0.6.0/trim_galore --paired --small_rna --dont_gzip -j 4 --ouput_dir --fastqc $output_folder $i

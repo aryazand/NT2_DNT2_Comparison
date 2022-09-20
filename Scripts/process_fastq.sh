246e4a24011942a8789d7630a816e70e8830347b
Arya Zand <28754724+aryazand@users.noreply.github.com>
2022-09-20 11:20:54 -0500

    script to trim fastq

--- Scripts/process_fastq.sh
+++ Scripts/process_fastq.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Get Arguments
+#   - i = input fastq file
+#   - o = output fasta file 
+while getopts i:o: flag
+do
+    case "${flag}" in
+        i) input_fastq=${OPTARG};;
+        o) output_fasta=${OPTARG};;
+    esac
+done
+
+mkdir -p ./Process_Data/Trimmed
+./Scripts/TrimGalore-0.6.0/trim_galore --paired --small_rna --dont_gzip -j 4 --ouput_dir --fastqc ./Process_Data/Trimmed $i

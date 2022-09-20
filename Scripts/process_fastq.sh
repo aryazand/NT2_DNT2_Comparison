884bcb4a304c8a6466048bfe5a4bcd163f014010
Arya Zand <28754724+aryazand@users.noreply.github.com>
2022-09-20 16:37:12 -0500

    fixed pattern input for trim_galore

--- Scripts/process_fastq.sh
+++ Scripts/process_fastq.sh
@@ -13,4 +13,4 @@ done
 shift $((OPTIND -1))
 
 mkdir -p $output_folder
-./Scripts/TrimGalore-0.6.0/trim_galore --paired --small_rna --dont_gzip -j 4 --output_dir $output_folder $input_fastq
+./Scripts/TrimGalore-0.6.0/trim_galore --paired --small_rna --dont_gzip -j 4 --output_dir $output_folder ${input_fastq[@]}
